25d6efd2cebd9b339cca38c4771b505edbffa274
Regin Hurley David <reginhurleydavid@MacBook-Air.local>
2021-10-07 18:22:01 -0600

    setting up the project

---
+++ Open-Office/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Open-Office
+//
+//  Created by Regin Hurley David on 10/7/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
